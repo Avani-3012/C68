bb806a74fe11bde1aeb652672ac087642534abee
Avani-3012 <65455563+Avani-3012@users.noreply.github.com>
2021-01-11 16:52:22 +0530

    Add files via upload

---
+++ screens/Facebook.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet,Image } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default class Facebook extends React.Component {
+    render(){
+      return(
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> FACEBOOK </Text>
+     </View>
+      );
+    }
+    }
+  
+
